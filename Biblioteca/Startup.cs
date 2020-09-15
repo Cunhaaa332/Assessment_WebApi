468d7afa935bd1488988a2d9da3c3f918d2964c0
Cunhaaa <gabriel.dsilveira@al.infnet.edu.br>
2020-09-15 19:20:56 -0300

    AutorController criado.

--- Biblioteca/Startup.cs
+++ Biblioteca/Startup.cs
@@ -23,10 +23,10 @@ namespace Biblioteca {
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services) {
+            services.AddControllers();
             services.AddDbContext<BibliotecaContext>(opt => {
                 opt.UseSqlServer(Configuration.GetConnectionString("BibliotecaConnection"));
             });
-            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
