ba1782d25b7f77e8c2f07a74d4e3791aacc42cc2
Cunhaaa <gabriel.dsilveira@al.infnet.edu.br>
2020-09-15 16:32:57 -0300

    Adicionar arquivos de projeto.

---
+++ Domain/Autor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain {
+    public class Autor {
+        public int Id { get; set; }
+        public String Nome { get; set; }
+        public String Sobrenome { get; set; }
+        public String Email { get; set; }
+        public DateTime Birth { get; set; }
+        public virtual IList<Livro> Livros { get; set; }
+    }
+}
