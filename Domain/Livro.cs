2ffc163d9ebf21247b0e4bbce4506ab4353ff5a4
Cunhaaa <gabriel.dsilveira@al.infnet.edu.br>
2020-09-15 16:32:57 -0300

    Models, Mapping e DbContext.

---
+++ Domain/Livro.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace Domain {
+    public class Livro {
+        public int Id { get; set; }
+        public String Titulo { get; set; }
+        public String ISBN { get; set; }
+        public string Ano { get; set; }
+
+        [JsonIgnore]
+        public virtual Autor Autor { get; set; }
+    }
+}
---
+++ Repository/Context/BibliotecaContext.cs
@@ -0,0 +1,26 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+using Repository.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository.Context {
+    public class BibliotecaContext : DbContext {
+        public DbSet<Autor> Autores { get; set; }
+        public DbSet<Livro> Livros { get; set; }
+
+        public BibliotecaContext(DbContextOptions<BibliotecaContext> options) : base(options) {
+
+        }
+
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            modelBuilder.ApplyConfiguration(new LivroMap());
+            modelBuilder.ApplyConfiguration(new AutorMap());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
