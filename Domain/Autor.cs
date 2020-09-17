7090d83109dda836d64d8c0e702a9091632f7342
Cunhaaa <gabriel.dsilveira@al.infnet.edu.br>
2020-09-17 14:40:23 -0300

    Att - Validação de Formulários.

--- Domain/Autor.cs
+++ Domain/Autor.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Domain {
@@ -8,6 +9,18 @@ namespace Domain {
         public String Nome { get; set; }
         public String Sobrenome { get; set; }
         public String Email { get; set; }
+        public DateTime Birth { get; set; }
+        public virtual IList<Livro> Livros { get; set; }
+    }
+    public class AutorResponse {
+        public int Id { get; set; }
+        [Required]
+        public String Nome { get; set; }
+        [Required]
+        public String Sobrenome { get; set; }
+        [Required]
+        public String Email { get; set; }
+        [Required]
         public DateTime Birth { get; set; }
         public virtual IList<Livro> Livros { get; set; }
     }
--- Domain/Livro.cs
+++ Domain/Livro.cs
@@ -1,23 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 using System.Text.Json.Serialization;
 
 namespace Domain {
     public class Livro {
-        public int Id { get; set; }
+        public int Id { get; set; } 
+        [Required]
         public String Titulo { get; set; }
+        [Required]
         public String ISBN { get; set; }
+        [Required]
         public string Ano { get; set; }
         [JsonIgnore]
         public virtual Autor Autor { get; set; }
     }
     public class LivroResponse {
         public int Id { get; set; }
+        [Required]
         public String Titulo { get; set; }
+        [Required]
         public String ISBN { get; set; }
+        [Required]
         public string Ano { get; set; }
-
         public virtual Autor Autor { get; set; }
     }
 
